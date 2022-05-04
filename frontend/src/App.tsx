c671fedfdc93f23b0a3c99d39c65bbebbb47d215
KonradRzodkiewicz <konradmmo@gmail.com>
2022-05-04 13:03:21 +0200

    Removed css folders&files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import './css/style.css';
+import './scss/style.scss';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import SignInScreen from './Screen/SignInScreen';
 import RegisterPopOut from './Shared/RegisterPopOut/RegisterPopOut';
