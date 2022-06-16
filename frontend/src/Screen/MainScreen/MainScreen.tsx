c19d216573d355c413d710e9f38e6186cbc29e67
Agnieszka bednarczyk <agnieszkabednar95@gmail.com>
2022-06-16 09:16:28 +0200

    Add Api.js

--- frontend/src/Screen/MainScreen/MainScreen.tsx
+++ frontend/src/Screen/MainScreen/MainScreen.tsx
@@ -1,52 +1,53 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import Grid from '@mui/material/Grid';
 import CustomCard from '../../Shared/Components/Card/Card';
 import {RecipeDTO} from '../../Shared/DTOs/RecipeDTO';
-import {UserDTO} from '../../Shared/DTOs/UserDTO';
+import Api from '../../Utils/Api'
 import './MainScreen.scss';
-import {IngredientDTO} from '../../Shared/DTOs/IngredientDTO';
 const MainScreen = () => {
-  const [dataAll, setDataAll] = useState<RecipeDTO[]>([]);
+  // const [dataAll, setDataAll] = useState<RecipeDTO[]>([]);
   useEffect(() => {
     console.log('Main screen');
-    const user: UserDTO = {
-      Name: 'Andrzej',
-      LastName: 'Kowalski',
-      Nickname: 'KowalSwojegoLosu',
-      Avatar: '',
-      Email: 'kowalski@tlen.pl',
-    };
-    const ingrid: IngredientDTO = {
-      Id: 0,
-      Amount: 100,
-      Unit: 'g',
-      Name: 'ryż',
-      Calories: 210,
-      Allergen: false,
-    };
-    const recipe: RecipeDTO = {
-      Id: 0,
-      Name: 'Pancakes',
-      Difficulty: 1,
-      Time: '',
-      Image: '',
-      Description:
-        // eslint-disable-next-line max-len
-        'Jajka roztrzepać i wymieszać z mlekiem, następnie połączyć z pozostałymi składnikami na końcu dodać masło. Odstawić na 15 minut.',
-      User: user,
-      Date: new Date('2022/05/24'),
-      Rating: 233,
-    };
-    const ingList: Array<IngredientDTO> = [];
-    for (let i = 0; i < 10; i++) {
-      ingrid.Id = i;
-      ingList.push(ingrid);
-    }
-    for (let i = 0; i < 10; i++) {
-      recipe.Id = i;
-      recipe.Ingredients = ingList;
-      setDataAll((v) => [...v, recipe]);
-    }
+    const a = await Api.
+    // const user: UserDTO = {
+    //   Name: 'Andrzej',
+    //   LastName: 'Kowalski',
+    //   Nickname: 'KowalSwojegoLosu',
+    //   Avatar: '',
+    //   Email: 'kowalski@tlen.pl',
+    // };
+    // const ingrid: IngredientDTO = {
+    //   Id: 0,
+    //   Amount: 100,
+    //   Unit: 'g',
+    //   Name: 'ryż',
+    //   Calories: 210,
+    //   Allergen: false,
+    // };
+    // const recipe: RecipeDTO = {
+    //   Id: 0,
+    //   Name: 'Pancakes',
+    //   Difficulty: 1,
+    //   Time: '',
+    //   Image: '',
+    //   Description:
+    //     // eslint-disable-next-line max-len
+    //     'Jajka roztrzepać i wymieszać z mlekiem, następnie połączyć
+    // z pozostałymi składnikami na końcu dodać masło. Odstawić na 15 minut.',
+    //   User: user,
+    //   Date: new Date('2022/05/24'),
+    //   Rating: 233,
+    // };
+    // const ingList: Array<IngredientDTO> = [];
+    // for (let i = 0; i < 10; i++) {
+    //   ingrid.Id = i;
+    //   ingList.push(ingrid);
+    // }
+    // for (let i = 0; i < 10; i++) {
+    //   recipe.Id = i;
+    //   recipe.Ingredients = ingList;
+    //   setDataAll((v) => [...v, recipe]);
+    // }
   }, []);
   return (
     <Grid container spacing={4} className="main">
