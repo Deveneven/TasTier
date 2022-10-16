0ad22299d3930767504f602f656cadc1580d978d
KonradRzodkiewicz <konradmmo@gmail.com>
2022-10-16 14:05:49 +0200

    Fixed styles of signin page, and changed mainTheme of buttons

--- frontend/src/Hooks/SignUp.js
+++
@@ -1,8 +0,0 @@
-import {auth} from '../Utils/Firebase';
-import {createUserWithEmailAndPassword} from 'firebase/auth';
-
-export const signup = (email, password) => {
-  return createUserWithEmailAndPassword(auth, email, password).catch((err) => {
-    throw new Error(err);
-  });
-};
