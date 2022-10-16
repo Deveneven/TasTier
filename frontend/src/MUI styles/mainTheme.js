80c066983a486f9621693767d6d46cbdd3711582
KonradRzodkiewicz <konradmmo@gmail.com>
2022-10-16 14:17:30 +0200

    Fixed styles of signin page, and changed mainTheme of buttons

--- frontend/src/MUI styles/mainTheme.js
+++ frontend/src/MUI styles/mainTheme.js
@@ -32,23 +32,3 @@ const theme = createTheme({
 
 export default theme;
 
-
-// import {createMuiTheme} from '@material-ui/core/styles';
-
-// const theme = createMuiTheme({
-
-//   palette: {
-// 	  primary: {
-//       main: 'hsl(28, 100%, 50%)',
-//       contrastText: 'hsl(0, 0%, 100%)',
-// 	  },
-// 	  secondary: {
-//       main: 'hsl(0, 0%, 100%)',
-//       contrastText: '#000',
-
-// 	  },
-
-//   },
-
-// });
-// export default theme;
