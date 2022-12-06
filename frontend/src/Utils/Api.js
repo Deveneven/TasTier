f776493441575cd1d9060a8519aa5cdd40ccd39f
KonradRzodkiewicz <konradmmo@gmail.com>
2022-12-06 18:19:49 +0100

    Added api to login

--- frontend/src/Utils/Api.js
+++ frontend/src/Utils/Api.js
@@ -1,28 +1,30 @@
 async function get(url) {
   await fetch(url, {
+    crossDomain: true,
     method: 'GET',
     headers: {
-      Accept: 'application/json',
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
     },
   }).then((response) => {
-    return JSON.parse(response);
+    return response;
   });
+  return response;
 }
 
 async function post(url, payload) {
   const response = await fetch(url, {
     crossDomain: true,
     method: 'POST',
-    credentials: 'same-origin',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    referrerPolicy: 'no-referrer',
     body: JSON.stringify(payload),
   }).then((response) => {
-    console.log(JSON.stringify(payload));
-    return response;
+    console.log(response);
+    return {value: response.text( (text) => {
+      return text.value;
+    }), status: response.status, ok: response.ok};
   });
   return response;
 }
