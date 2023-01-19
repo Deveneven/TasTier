a24841c66ef901b505fd7851c5cb74c9dd16e70c
Agnieszka bednarczyk <agnieszkabednar95@gmail.com>
2023-01-19 17:38:29 +0100

    Merge correction

--- frontend/src/Shared/Components/RecipesList/RecipesList.tsx
+++ frontend/src/Shared/Components/RecipesList/RecipesList.tsx
@@ -13,61 +13,58 @@ const RecipesList = (props: Props) => {
   const [fiteredData, setFilteredData] = useState<RecipeDTO[]>([]);
 
   useEffect(() => {
-    setFilteredData(props.dataAll);
+    if (props.dataAll) {
+      setFilteredData(props.dataAll);
+    }
   }, [props.dataAll]);
 
   const onSearch = (input: string) => {
-    console.log('on search');
-    console.log(input);
-    if (input.length > 3) {
+    if (props.dataAll && input.length > 3) {
       const computedData = props.dataAll.filter((elem) => elem.name.toLocaleLowerCase().includes(input));
       setFilteredData(computedData);
-    } else if (input == '') {
+    } else if (props.dataAll && input == '') {
       setFilteredData(props.dataAll);
     }
   };
   const onFilter = (elem) => {
-    console.log('On filter');
-    console.log(elem);
     let computedData = props.dataAll;
-    console.log(computedData);
-    if (elem.tags.liked.length > 0) {
-      computedData = computedData.filter((x) => x.tags.some((y) => elem.tags.liked.includes(y.tagName)));
+    if (computedData) {
+      if (elem.tags.liked.length > 0) {
+        computedData = computedData.filter((x) => x.tags.some((y) => elem.tags.liked.includes(y.tagName)));
+      }
+      if (elem.tags.disliked.length > 0) {
+        computedData = computedData.filter((x) => !x.tags.some((y) => elem.tags.disliked.includes(y.tagName)));
+      }
+      if (elem.cousine.liked.length > 0) {
+        computedData = computedData.filter((x) => elem.cousine.liked.includes(x.cousine));
+      }
+      if (elem.cousine.disliked.length > 0) {
+        computedData = computedData.filter((x) => !elem.cousine.disliked.includes(x.cousine));
+      }
+      if (elem.ingredients.liked.length > 0) {
+        computedData = computedData.filter((x) => x.ingredients.some((y) => elem.ingredients.liked.includes(y.name)));
+      }
+      if (elem.ingredients.disliked.length > 0) {
+        computedData = computedData.filter((x) => !x.ingredients.some((y) => elem.ingredients.disliked.includes(y.name)));
+      }
+      if (elem.allergens.liked.length > 0) {
+        computedData = computedData.filter((x) => x.ingredients.some((y) => elem.allergens.liked.includes(y.allergen_name)));
+      }
+      if (elem.allergens.disliked.length > 0) {
+        computedData = computedData.filter((x) => !x.ingredients.some((y) => elem.allergens.disliked.includes(y.allergen_name)));
+      }
+      setFilteredData(computedData);
     }
-    if (elem.tags.disliked.length > 0) {
-      computedData = computedData.filter((x) => !x.tags.some((y) => elem.tags.disliked.includes(y.tagName)));
-    }
-    if (elem.cousine.liked.length > 0) {
-      computedData = computedData.filter((x) => elem.cousine.liked.includes(x.cousine));
-    }
-    if (elem.cousine.disliked.length > 0) {
-      computedData = computedData.filter((x) => !elem.cousine.disliked.includes(x.cousine));
-    }
-    if (elem.ingredients.liked.length > 0) {
-      computedData = computedData.filter((x) => x.ingredients.some((y) => elem.ingredients.liked.includes(y.name)));
-    }
-    if (elem.ingredients.disliked.length > 0) {
-      computedData = computedData.filter((x) => !x.ingredients.some((y) => elem.ingredients.disliked.includes(y.name)));
-    }
-    if (elem.allergens.liked.length > 0) {
-      computedData = computedData.filter((x) => x.ingredients.some((y) => elem.allergens.liked.includes(y.allergen_name)));
-    }
-    if (elem.allergens.disliked.length > 0) {
-      computedData = computedData.filter((x) => !x.ingredients.some((y) => elem.allergens.disliked.includes(y.allergen_name)));
-    }
-    console.log(computedData);
-    setFilteredData(computedData);
   };
   return (
-      <BaseLayout onSearch={onSearch} onFilter={onFilter}>
-          {props.children}
+    <BaseLayout onSearch={onSearch} onFilter={onFilter}>
+      {props.children}
       {fiteredData?.map((recipe: RecipeDTO) => {
         return (
           <CustomCard key={recipe.id} data={recipe} />
         );
-      }):
- props.recipeSingle && ( <CustomCard key={props.recipeSingle?.id} data={props.recipeSingle} />)
-      }
+      })}
+      {props.recipeSingle && ( <CustomCard key={props.recipeSingle?.id} data={props.recipeSingle} />)}
     </BaseLayout >
   );
 };
